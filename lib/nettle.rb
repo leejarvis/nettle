1c42b7a06841a1738211cf8891ebabe523c1677a
Lee Jarvis <ljjarvis@gmail.com>
2013-01-03 13:33:32 +0000

    initial commit

---
+++ lib/nettle.rb
@@ -0,0 +1,31 @@
+require 'net/http'
+require 'uri'
+
+require 'nettle/verbs'
+
+module Nettle
+  module_function
+
+  include Verbs
+  extend Verbs
+
+  def request(method, url, options = {})
+    uri = URI.parse(url)
+    http = Net::HTTP.new(uri.host, uri.port)
+    request = make_request(method, uri, options)
+    yield request if block_given?
+    response = http.request(request)
+    Array(options[:response_extensions]).each { |ext| response.extend(ext) }
+    response
+  end
+
+
+  def make_request(method, uri, options)
+    klass = Net::HTTP.const_get(method.to_s.downcase.capitalize)
+    request = klass.new(uri.request_uri)
+    request
+  rescue NameError
+    raise NameError, "Unknown request method #{method}"
+  end
+
+end
---
+++ lib/nettle/verbs.rb
@@ -0,0 +1,44 @@
+module Nettle
+  module Verbs
+    def get(url, options = {}, &block)
+      request(:Get, url, options, &block)
+    end
+
+    def head(url, options = {}, &block)
+      request(:Head, url, options, &block)
+    end
+
+    def post(url, params = {}, options = {}, &block)
+      request(:Post, url, options) do |req|
+        req.set_form_data params
+        yield req if block_given?
+      end
+    end
+
+    def put(url, params = {}, options = {}, &block)
+      request(:Put, url, options) do |req|
+        req.set_form_data params
+        yield req if block_given?
+      end
+    end
+
+    def patch(url, params = {}, options = {}, &block)
+      request(:Patch, url, options) do |req|
+        req.set_form_data params
+        yield req if block_given?
+      end
+    end
+
+    def delete(url, options = {}, &block)
+      request(:Head, url, options, &block)
+    end
+
+    def trace(url, options = {}, &block)
+      request(:Head, url, options, &block)
+    end
+
+    def options(url, options = {}, &block)
+      request(:Get, url, options, &block)
+    end
+  end
+end
